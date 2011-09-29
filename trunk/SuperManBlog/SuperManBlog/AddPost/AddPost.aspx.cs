b2affb73ea099500fb3c28a674fdaa8fba5131c3
pavlo.sobchuk@gmail.com <pavlo.sobchuk@gmail.com@46a3702f-a4e4-eca2-0192-2cb49bf4b000>
2011-09-29 18:30:37 +0000

    додавання посту (сторінка AddPost.aspx) + змінено Web.config (я зняв валідацію html тегів в textbox для того щоб нормально працював редактор тексту)

---
+++ trunk/SuperManBlog/SuperManBlog/AddPost/AddPost.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace AddPost
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (topic.Text == "") Response.Write("Введіть назву суперпосту");
+            
+            else
+            {
+                XmlDocument xml_doc = new XmlDocument();
+                xml_doc.Load(Server.MapPath("../SuperPosts.xml"));
+
+                XmlNode newNode = xml_doc.DocumentElement.FirstChild;
+
+                XmlElement newElem = xml_doc.CreateElement("post");
+                newElem.SetAttribute("topic", topic.Text);
+                newElem.InnerText = txtNotes.Text;
+               
+                xml_doc.DocumentElement.InsertBefore(newElem, newNode);
+
+                xml_doc.Save(Server.MapPath("../SuperPosts.xml"));
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            txtNotes.Text = "";
+            topic.Text = "";
+        }
+    }
+}
